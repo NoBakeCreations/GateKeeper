f85ead232699488382268183516653fe8ab04e6f
Seth Cook <sethcook@purdue.edu>
2017-03-03 15:20:37 -0500

    Prototype for handling multiple back servers.

--- src/main/java/com/galacticcookiejar/nobakecreation/gatekeeper/Server.java
+++ src/main/java/com/galacticcookiejar/nobakecreation/gatekeeper/Server.java
@@ -9,12 +9,18 @@ import java.lang.Thread;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
 
-  public Server() {
+  Map<String,Integer> proxyMapping;
 
+  public Server() {
+    this.proxyMapping = new HashMap<String, Integer>();
+    this.proxyMapping.put("mctest1.galacticcookiejar.com", 5001);
+    this.proxyMapping.put("mctest2.galacticcookiejar.com", 5002);
   }
 
   public void start() throws IOException {
@@ -33,24 +39,33 @@ public class Server {
     while( ! server_handler.isClosed() ) {
       try {
         client_connection = server_handler.accept();
-        Socket server_connection = new Socket("localhost", 5001);
 
         MinecraftInputStream client_mc_input = new MinecraftInputStream(client_connection.getInputStream());
         HandshakePacket client_handshake = (HandshakePacket)client_mc_input.getMinecraftPacket(
             HandshakePacket.State.HANDSHAKING,
             HandshakePacket.BoundTowards.SERVER);
         System.out.println("Connecting to server: " + client_handshake.getServerAddress() + ":" + client_handshake.getServerPort());
-        client_handshake.writeData(new MinecraftOutputStream(server_connection.getOutputStream()));
 
-        ProxyThread t1 = new ProxyThread(client_connection, server_connection, client_connection.getInputStream(), server_connection.getOutputStream());
-        ProxyThread t2 = new ProxyThread(client_connection, server_connection, server_connection.getInputStream(), client_connection.getOutputStream());
-        t1.start();
-        t2.start();
+        if( this.proxyMapping.containsKey(client_handshake.getServerAddress()) ) {
+          int server_port = this.proxyMapping.get(client_handshake.getServerAddress());
+          Socket server_connection = new Socket("localhost", server_port);
 
-        connections.add(new Socket[] {server_connection, server_connection});
-        proxy_threads.add(new Thread[] {t1, t2});
+          client_handshake.writeData(new MinecraftOutputStream(server_connection.getOutputStream()));
 
-        System.out.println("Accepted a new connection!");
+          ProxyThread t1 = new ProxyThread(client_connection, server_connection, client_connection.getInputStream(), server_connection.getOutputStream());
+          ProxyThread t2 = new ProxyThread(client_connection, server_connection, server_connection.getInputStream(), client_connection.getOutputStream());
+
+          t1.start();
+          t2.start();
+
+          connections.add(new Socket[] {server_connection, server_connection});
+          proxy_threads.add(new Thread[] {t1, t2});
+
+          System.out.println("Accepted a new connection!");
+        } else {
+          System.out.println("Unable to connect to server, not found in mapping.");
+          client_connection.close();
+        }
       } catch(Exception err) {
         System.out.println("Connection failed: " + err);
       }
